d56173b4a7913301f6a9151e9f7784c240b041d1
Martynas <marcius989@gmail.com>
2024-06-08 16:28:50 +0300

    Changes theme

--- assets/us-map.js
+++ assets/us-map.js
@@ -3,20 +3,14 @@ $("path, circle").hover(function(e) {
 	$('#info-box').html($(this).data('info'));
 });
 
-$("path, circle").mouseleave(function(e) {
-	$('#info-box').css('display','none');
+$("path, circle").on('click touchend', function(e) {
+	var stateInitials = $(this).attr('id');
+	var link = `products/${stateInitials.toLowerCase()}`;
+	window.open(link,'_blank');
+	return false;
 });
 
-// $(document).mousemove(function(e) {
-// 	$('#info-box').css('top',e.pageY-$('#info-box').height()-30);
-// 	$('#info-box').css('left',e.pageX-($('#info-box').width())/2);
-// }).mouseover();
-
-var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-if(ios) {
-	$('a').on('click touchend', function() {
-		var link = $(this).attr('href');
-		window.open(link,'_blank');
-		return false;
-	});
-}
+$("path, circle").mouseleave(function(e) {
+	$('#info-box').css('display','none');
+	console.log("test");
+});
